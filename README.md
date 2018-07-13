947bf8f3bf69874b370908d3ebf48877ce99f78b
vervin <vervin@163.com>
2018-07-13 18:23:36 +0800

    webpack4.0

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-> https://phoebecodespace.github.io/2018/07/04/webpack-4-tutorial/
 webpack 4 ：从0配置到项目搭建
+
+
+### webpack 4 的零配置主要应用于：
+
+1. entry 默认设置为 ./src/index.js
+2. output 默认设置为 ./dist/main.js
+3. production 和 development 两种模式
+
+### 项目搭建，我们对webpack的诉求是：
+
+1. js的处理：转换 ES6 代码，解决浏览器兼容问题
+
+``` npm i babel-core babel-loader babel-preset-env --save-dev ```
+
+2. css的处理：编译css，自动添加前缀，抽取css到独立文件
+3. html的处理：复制并压缩html文件
+4. dist的清理：打包前清理源目录文件
+5. assets的处理：静态资源处理
+6. server的启用：development 模式下启动服务器并实时刷新
+
+### 感谢 
+> https://phoebecodespace.github.io/2018/07/04/webpack-4-tutorial/
+
+
---
+++ webpack-4-quickstart/webpack.config.js
@@ -0,0 +1,62 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+module.exports = (env, argv) => {
+    const devMode = argv.mode !== 'production'
+    return {
+        entry: [
+            "babel-polyfill",
+            path.join(__dirname, './src/index.js')
+        ],
+        module: {
+
+            rules: [{
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader"
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'postcss-loader'
+                    ]
+                },
+                {
+                    test: /\.html$/,
+                    use: [{
+                        loader: "html-loader",
+                        options: {
+                            minimize: true
+                        }
+                    }]
+                },
+                {
+                    test: /\.(png|jpg|gif)$/,
+                    use: [
+                      {
+                        loader: 'file-loader',
+                        options: {}
+                      }
+                    ]
+                  }
+            ]
+        },
+        plugins: [
+            new HtmlWebPackPlugin({
+                template: "./src/index.html",
+                filename: "./index.html"
+            }),
+            new MiniCssExtractPlugin({
+                filename: "[name].css",
+                chunkFilename: "[id].css"
+            }),
+
+            new CleanWebpackPlugin(['dist']),
+        ]
+    }
+
+}
